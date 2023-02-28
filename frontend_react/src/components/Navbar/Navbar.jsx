741440210ebc01c2ec742b4ca441973780df3883
Èric Terrisse <118490581+ericterrisse@users.noreply.github.com>
2023-02-28 11:50:38 -0500

    final portfolio base

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
           >
             <ul>
               <HiX onClick={() => setToggle(false)} />
-              {["home", "about", "work", "skills", "contact"].map((item) => (
+              {["home", "about", "work", "skills","testimonials",  "contact"].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
